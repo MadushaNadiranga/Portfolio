267b673ed7ef0a8e55eb50c67075a3ae5d560783
MadushaNadiranga <lmmadushanadiranga@gmail.com>
2025-11-18 23:37:16 +0530

    Created: HomePage

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import LogoSection from "./sections/LogoSection";
+import NavBar from "./componants/NavBar";
+import FeatureCards from "./sections/FeatureCards";
+import Hero from "./sections/hero";
+import ShowcaseSection from "./sections/ShowcaseSection";
+
+const App = () => {
+  return (
+    <>
+        <NavBar/>
+        <Hero/>
+        <ShowcaseSection/>
+        <LogoSection/>
+        <FeatureCards/>
+    </>
+  );
+};
+
+export default App;
