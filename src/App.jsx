1c5767568b366372637c2ac6a330b36ae7dfc769
MadushaNadiranga <lmmadushanadiranga@gmail.com>
2025-11-21 22:02:38 +0530

    Created: Contact Page

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import ShowcaseSection from "./sections/ShowcaseSection";
 import ExperinceSection from "./sections/ExperienceSection";
 import TechStack from "./sections/TechStack";
 import Testimonials from "./sections/Testimonials";
+import Contact from "./sections/Contact";
+
 
 const App = () => {
   return (
@@ -18,6 +20,7 @@ const App = () => {
         <ExperinceSection/>
         <TechStack/>
         <Testimonials />
+        <Contact />
     </>
   );
 };
