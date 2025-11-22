a94546fdefa399c51b4ccdd6362f851f937cf187
MadushaNadiranga <lmmadushanadiranga@gmail.com>
2025-11-22 21:34:28 +0530

    Updated: Hero Section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -51,8 +51,10 @@ const Hero = () => {
                     <h1>That Deliver Results</h1>
                 </div>
                 <p className="text-white-50 md:text-xl relative z-10 pointer-events-none">
-                    Hi, I'm Madusha. a Developer based in Srilanka with a passion for code.
+                    Hi, I'm Madusha Nadiranga<br />
+                    A Software Developer<br />
                 </p>
+
                 <Button className="md:w-80 md:h-16 w-60 h-12" id="button" text="See My Work"/>
             </div>
             </header>
